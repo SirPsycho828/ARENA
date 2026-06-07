5aba92f61bf5433e6bfb24fc4824b39f6b036935
Steve Petusky <steve.petusky@gmail.com>
2026-06-07 18:08:42 -0400

    fix: move landing image behind content so text stays fully opaque

--- client/src/components/LandingPage.tsx
+++ client/src/components/LandingPage.tsx
@@ -332,8 +332,8 @@ export function LandingPage({ onEnter }: LandingPageProps) {
         <div className="absolute bottom-16 left-6 w-8 h-8 border-l-2 border-b-2 border-primary/20" />
         <div className="absolute bottom-16 right-6 w-8 h-8 border-r-2 border-b-2 border-accent/20" />
 
-        {/* Landing image overlay */}
-        <div className="absolute inset-0 z-20 pointer-events-none opacity-50">
+        {/* Landing image background */}
+        <div className="absolute inset-0 z-[1] pointer-events-none opacity-50">
           <img
             src="/images/AgentProfiles/ARENA_landing.png"
             alt=""
